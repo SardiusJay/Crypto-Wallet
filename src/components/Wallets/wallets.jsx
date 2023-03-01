4be1e3e55e48115be3a8e261505038d86003c692
SardiusJay <abidoyejay@gmail.com>
2023-03-01 15:40:08 +0100

    wip:Changing some content

--- src/components/Wallets/wallets.jsx
+++ src/components/Wallets/wallets.jsx
@@ -3,7 +3,9 @@ import { RiSettings5Fill } from 'react-icons/ri';
 
 const wallets = () => {
   return (
-    <div>
+    <div className="wallet">
+    <h1>Connect Wallet</h1>
+    <p>Open protocol for connecting Wallets to Dapps</p>
       
     </div>
   )
