a7ca63cd6e2684d39fe582ae9dbb2fd82f125bfc
SardiusJay <abidoyejay@gmail.com>
2023-03-03 00:23:25 +0100

    Adding the images

--- src/components/Wallets/wallets.jsx
+++ src/components/Wallets/wallets.jsx
@@ -7,6 +7,12 @@ const Wallets = () => {
     <h1>Connect Wallet</h1>
     <p>Open protocol for connecting Wallets to Dapps</p>
     <div classNme="wallet-crypto-image">
+    <img src="/Image/brd.jpg" alt="BRD wallet" />
+    <img src="/Image/Coinbaselogo_Supplied_250x250-2.png" alt="BRD wallet" />
+    <img src="/Image/saitama.png" alt="BRD wallet" />
+    <img src="/Image/terra.png" alt="BRD wallet" />
+    <img src="/Image/phantom.jpeg" alt="BRD wallet" />
+    
     </div>
       
     </div>
