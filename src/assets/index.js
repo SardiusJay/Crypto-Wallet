f6394bfbdc6d794206147fcbf8af0483fd778111
Faith Adebayo <63503378+Dev-Faith@users.noreply.github.com>
2023-03-05 23:31:13 +0100

    finished pqge two 🚀🌘

--- src/assets/index.js
+++ src/assets/index.js
@@ -28,6 +28,48 @@ import formatic from "./Image/formatic.jpeg";
 import bitpay from "./Image/bitpay.jpeg";
 import autherium from "./Image/autherium.jpeg";
 import math from "./Image/math.jpeg";
+import oneinch from "./Image/oneinch.avif";
+import loopring from "./Image/loopring.avif";
+import huobi from "./Image/Huobi.avif";
+import eido from "./Image/Eido.jpeg";
+import aktion from "./Image/aktionarirat.jpeg";
+import alice from "./Image/Alice.jpeg";
+import alpha from "./Image/AlphaWallet.jpeg";
+import atomic from "./Image/Atomic.jpeg";
+import atwallet from "./Image/atwallet.jpeg";
+import binana from "./Image/Binana.jpeg";
+import bitkeep from "./Image/bitkeep.avif";
+import bridge from "./Image/bridgeWallet.jpeg";
+import coin98 from "./Image/coin98.jpeg";
+import coinmoni from "./Image/coinmoni.png";
+import cybavo from "./Image/cybavo.jpeg";
+import decent from "./Image/decent.jpeg";
+import dok from "./Image/dok.jpeg";
+import ellipal from "./Image/ellipal.jpeg";
+import gridplus from "./Image/gridplus.jpeg";
+import infinito from "./Image/ifinito.jpeg";
+import keypringPro from "./Image/keypringPro.png";
+import kyberswap from "./Image/kyberswap.avif";
+import linen from "./Image/linen.jpeg";
+import midas from "./Image/midas.jpeg";
+import nash from "./Image/nash.avif";
+import ownbit from "./Image/ownbit.jpeg";
+import payTube from "./Image/payTube.jpeg";
+import peak from "./Image/peak.jpeg";
+import Rwallet from "./Image/Rwallet.jpeg";
+import safePal from "./Image/safePal.png";
+import spark from "./Image/spark.jpeg";
+import spatium from "./Image/spatium.jpeg";
+import talken from "./Image/talken.jpeg";
+import tokenary from "./Image/tokenary.jpeg";
+import torus from "./Image/torus.jpeg";
+import tron from "./Image/tron.png";
+import trustVault from "./Image/trustVault.jpeg";
+import unstoppable from "./Image/unstoppable.jpeg";
+import viaWallet from "./Image/viaWallet.jpeg";
+import vision from "./Image/vision.jpeg";
+import walletio from "./Image/walletio.jpeg";
+import zelcore from "./Image/ZelCore.jpeg";
 
 export {
   brd,
@@ -60,4 +102,46 @@ export {
   bitpay,
   autherium,
   math,
+  oneinch,
+  loopring,
+  huobi,
+  eido,
+  aktion,
+  alice,
+  alpha,
+  atomic,
+  atwallet,
+  binana,
+  bitkeep,
+  bridge,
+  coin98,
+  coinmoni,
+  cybavo,
+  decent,
+  dok,
+  ellipal,
+  gridplus,
+  infinito,
+  keypringPro,
+  kyberswap,
+  linen,
+  midas,
+  nash,
+  ownbit,
+  payTube,
+  peak,
+  Rwallet,
+  safePal,
+  spark,
+  spatium,
+  talken,
+  tokenary,
+  torus,
+  tron,
+  trustVault,
+  unstoppable,
+  viaWallet,
+  vision,
+  walletio,
+  zelcore,
 };
